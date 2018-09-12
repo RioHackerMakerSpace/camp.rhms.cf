faa24a06debfa13b7293720829cf25536a06edab
Niv Sardi <xaiki@evilgiggle.com>
2018-09-12 12:45:37 -0300

    sexyer agenda
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/containers/Event.js
+++ src/containers/Event.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Markdown from 'react-markdown'
 
-const Event = ({start, summary, description, location}) => {
+const Event = ({start, summary, description, location, slides}) => {
     let s = start.toString()
     let d;
     try {
@@ -12,20 +12,19 @@ const Event = ({start, summary, description, location}) => {
     }
 
     return (
-        <div className="event extend">
-            <div className="event-time">{d}<span></span></div>
-            <div className="event-info">
-                <div>
-                    <h4>{summary}</h4>
-                    <p className="speaker"><strong>
-                        {location}
-                    </strong></p>
-
-                    <Markdown source={description}/>
-                    <p className="speaker permanent"> <a href="/data/slides/tompits.pdf" target="_blank">» Slides</a> </p>
+            <div className={`event ${description ? 'extend' : ''}`}>
+                <div className="event-time">{d}<span></span></div>
+                <div className="event-info">
+                    <div>
+                        <h4>{summary}</h4>
+                        {location &&  <p className="speaker"><strong>
+                            {location}
+                        </strong></p>}
+                        {description && <Markdown source={description}/>}
+                        {slides && <p className="speaker permanent"> <a href="/data/slides/tompits.pdf" target="_blank">» Slides</a> </p>}
+                    </div>
                 </div>
             </div>
-        </div>
     )
 }
 
