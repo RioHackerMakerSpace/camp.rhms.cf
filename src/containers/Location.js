10e890e713deddbef58fefc5a9073f75b8d39081
Niv Sardi <xaiki@evilgiggle.com>
2018-09-13 19:44:14 -0300

    get mais portunhol
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/containers/Location.js
+++ src/containers/Location.js
@@ -10,12 +10,12 @@ const Place = ({nome, endereco, city, state}) => (
 )
 
 const Location = ({place: {image, location, city, state, country, description}, housing}) => (
-                                                                                              <div className="location section imgover" style={{
-                                                                                                                                                backgroundImage: `url(${image.url})`
+    <div className="location section imgover" style={{
+        backgroundImage: `url(${image.url})`
     }}>
         <div className="container">
 
-            <h2>Location and Venue</h2>
+            <h2>Localização</h2>
             <p className="subtitle">{location} &ndash; {city} &ndash; {state} &ndash; {country}</p>
 
             <div className="info">
@@ -29,15 +29,12 @@ const Location = ({place: {image, location, city, state, country, description}, 
                 <div className="address">
                     <h4>{description}</h4>
 
-                    <h5><i className="fa fa-chevron-right"></i> Venue</h5>
+                    <h5><i className="fa fa-chevron-right"></i>Local</h5>
                     <Place {...housing[0]}/>
 
-                    <h5><i className="fa fa-chevron-right"></i> Accommodation</h5>
+                    <h5><i className="fa fa-chevron-right"></i>Acomodações</h5>
                     {housing.slice(1).map(p => <Place {...p}/>)}
 
-                    <h5><i className="fa fa-chevron-right"></i> Additional details</h5>
-
-                    <p>We have collected the information for guests <a href="/info">on a separate page</a>.</p>
                 </div>
 
             </div>
--- static.config.js
+++ static.config.js
@@ -57,7 +57,7 @@ const parseDate = (date) => {
   const year = date.substr(0, 4);
   const month = parseInt(date.substr(4, 2), 10) - 1;
   const day = date.substr(6, 2);
-  const hour = date.substr(9, 2);
+  const hour = date.substr(9, 2) - 3;
   const minute = date.substr(11, 2);
   const second = date.substr(13, 2);
 
@@ -116,7 +116,7 @@ export default {
         },
         place: {
           location: 'Jardim 5.0',
-          url: 'https://www.google.com/maps/@-22.3986366,-43.3664379,3a,51.7y,182.44h,89.42t/data=!3m6!1e1!3m4!1sQzRNnqnN6pHR61Iz0sDzsw!2e0!7i13312!8i6656?shorturl=1'
+          url: 'https://www.google.com/maps/@-22.3986366,-43.3664379,3a,51.7y,182.44h,89.42t/data=!3m6!1e1!3m4!1sQzRNnqnN6pHR61Iz0sDzsw!2e0!7i13312!8i6656?shorturl=1',
           city: 'Paty Do Alferes',
           state: 'RJ',
           country: 'Brasil',
